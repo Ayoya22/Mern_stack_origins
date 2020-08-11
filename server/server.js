8bda5c0911e851a4caec1313f89f333feddfbc6b
Ayoya22 <djoukamsimplice@gmail.com>
2020-08-11 11:01:43 +0100

    Fixed Errors
    Signed-off-by: Ayoya22 <djoukamsimplice@gmail.com>

--- server/server.js
+++ server/server.js
@@ -4,19 +4,23 @@ import path from 'path'
 import template from './../template'
 import { MongoClient } from 'mongodb'
 
-const CURRENT_WORKING_DIR = process.cwd()
 const app = express()
+
 devBundle.compile(app)/*should be commented out when building the application code for production.*/
+
+const CURRENT_WORKING_DIR = process.cwd()
+
 app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
 app.get('/', (req, res) => {
     res.status(200).send(template())
 })
 
-let port = process.env.PORT || 3000
+let port = process.env.PORT || 3030
 app.listen(port, function onStart(err) {
     if (err) {
         console.log(err)
     }
+    console.info('Server started on port %s.', port)
 })
 
 const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/mernHICCAM'
